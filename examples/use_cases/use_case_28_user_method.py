0f265a9202d6fdac89b36cbcfc6b0156b100fd0e
Radek Poleski <radek.poleski@gmail.com>
2018-07-18 11:32:51 -0400

    UC28 update

--- examples/use_cases/use_case_28_user_method.py
+++ examples/use_cases/use_case_28_user_method.py
@@ -1,8 +1,9 @@
 """
-The method should be set by the user to Model class. And then passed to 
-ModelParamters. Together with it, we should pass other important information. 
+The method should be set by the user to Model class. And then passed to
+MagnificationCurve. Together with it, we should pass other important
+information. 
 
-The ModelParamters.__init__ has:
+The MagnificationCurve.__init__ has:
 times, parameters, parallax=None, coords=None, satellite_skycoord=None, gamma=0.
 This are used to produce:
 Trajectory
@@ -19,6 +20,9 @@ from MulensModel import binarylens
 
 binarylens.vbbl.VBBinaryLensing_BinaryMagDark_2.argtypes = 5 * [ctypes.c_double]
 binarylens.vbbl.VBBinaryLensing_BinaryMagDark_2.restype = ctypes.c_double
+
+# expected: 1.63603122 ; for LD (coeff_u = 10.) 1.6366862
+print(binarylens.vbbl.VBBinaryLensing_BinaryMagDark_2(s, q, 0.659856030318, -0.0528038964219, rho))
 # The function:
 # binarylens.vbbl.VBBinaryLensing_BinaryMagDark_2 
 # has following paramters:
